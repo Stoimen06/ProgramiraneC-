5e2a2c250dab3fe3a5d13671957153d71d367dc9
Stoimen06 <99276722+Stoimen06@users.noreply.github.com>
2022-02-22 20:46:34 +0200

    Add files via upload

---
+++ 22.02dom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _22._02_dom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hex = "12F";
+            int dec = Convert.ToInt32(hex, 16);
+            int dec1 = 12;
+            int suma = dec + dec1;
+            string suma2 = Convert.ToString(suma, 16).ToUpper();
+            Console.WriteLine(dec + "(16)+ " + dec1 + "(10)" + " = " + suma2);
+
+            string hex1 = "12F";
+            int dec3 = Convert.ToInt32(hex1, 16);
+            int dec4 = 12;
+            int suma5 = dec3 - dec4;
+            Console.WriteLine(dec3 + "(16)- " + dec4 + "(10)" + " = " + +suma5);
+        }
+    }
+}
