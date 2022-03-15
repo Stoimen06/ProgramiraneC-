3bfb695820a09a1fd521d8715f41b170aa073617
Stoimen06 <99276722+Stoimen06@users.noreply.github.com>
2022-03-15 17:31:46 +0200

    Add files via upload

---
+++ 15.03 dom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _15._03_dom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vtornik");
+            Console.WriteLine("Vyvedete broq na chasovete");
+            int n = int.Parse(Console.ReadLine());
+            string[] chasove = new string[n];
+            for (int i = 0; i < chasove.Length; i++)
+            {
+                Console.WriteLine("Vyvedete predmet");
+                chasove[i]=Console.ReadLine();
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(i+ " "+chasove[i]); 
+            }
+        }
+    }
+}
