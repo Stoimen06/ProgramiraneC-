6c28bcbaa91a0c1dc3444aa0b6c9cee54cc98e37
Stoimen06 <99276722+Stoimen06@users.noreply.github.com>
2022-04-15 17:39:31 +0300

    Add files via upload

---
+++ 14.04. dom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _14._04._dom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> producti = new List<string>();
+            producti.Add("mlqko");
+            producti.Add("elda");
+            Console.WriteLine("Productite pyrwonachalno sa:");
+            for (int i = 0; i < producti.Count; i++)
+            {
+                Console.WriteLine(producti[i]);
+            }
+            for (int i = 0; i < producti.Count; i++)
+            {
+                if (producti[i] == "elda")
+                {
+                    Console.WriteLine("Da ima elda");
+                }
+            }
+            Console.WriteLine(producti.Contains("elda"));
+            producti.Insert(2,"duner");
+            Console.WriteLine("Spisyka s dobawen duner");
+            for (int i = 0; i < producti.Count; i++)
+            {
+                Console.WriteLine(producti[i]);
+            }
+            Console.WriteLine("Productite w kraq sa");
+            for (int i = 0; i < producti.Count; i++)
+            {
+                Console.WriteLine(producti[i]);
+            }
+        }
+    }
+}
