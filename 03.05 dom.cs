b925b0ab4430e4675be7f33924a4464a67c7a2ad
Stoimen06 <99276722+Stoimen06@users.noreply.github.com>
2022-05-04 15:30:52 +0300

    Add files via upload

---
+++ 03.05 dom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._05_dom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имена и височина (в м) на ученици:");
+            Dictionary<string, double> visochina = new Dictionary<string, double>();
+            visochina.Add("Mladen", 1.68);
+            visochina.Add("Ibraim", 1.76);
+            foreach (var i in visochina)
+            {
+
+                Console.WriteLine(i + " м.");
+            }
+            double avg = visochina.Values.Average();
+            Console.WriteLine("Средната височина е: " + avg + " м.");
+        }
+    }
+}
